ed58bb944e1605c0b2224ed8d1b51edc6606680d
shafiya <inamdarshafiya463@gmail.com>
2022-03-16 17:36:29 +0530

    all components updated

--- EducationSystem/src/app/app.module.ts
+++ EducationSystem/src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { CourseComponent } from './course/course.component';
+import { TeacherComponent } from './teacher/teacher.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    CourseComponent,
+    TeacherComponent,
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
